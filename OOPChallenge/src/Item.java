8c9bad3c805707e41eb87887f058d7c2c5d0c77b
jul1998 <jguevara321@gmail.com>
2023-08-24 23:55:51 -0600

    nEW PROJECT

--- OOPChallenge/src/Item.java
+++ OOPChallenge/src/Item.java
@@ -1,48 +1,47 @@
 public class Item {
 
-    private String name;
     private String type;
+    private String name;
     private double price;
-    private String size;
+    private String size = "MEDIUM";
 
-    public Item(String name, String type, double price, String size) {
-        this.name = name;
-        this.type = type;
+    public Item(String type, String name, double price) {
+        this.type = type.toUpperCase();
+        this.name = name.toUpperCase();
         this.price = price;
-        this.size = size;
     }
 
-    public String getName(){
-        if (this.type.equals("SIDE") || type.equals("DRINK") ){
+    public String getName() {
+
+        if (type.equals("SIDE") || type.equals("DRINK")) {
             return size + " " + name;
         }
+
         return name;
     }
 
-    public double getBasePrice(){
+    public double getBasePrice() {
         return price;
     }
 
-    public double getAdjustedPrice(){
-        return switch(this.size) {
+    public double getAdjustedPrice() {
+        return switch (size) {
             case "SMALL" -> getBasePrice() - 0.5;
             case "LARGE" -> getBasePrice() + 1;
             default -> getBasePrice();
         };
-        }
-
-        public void setSize(String size){
-            this.size = size;
-        }
-
-        public static void printItem(String name, double price) {
-            System.out.println(name + " - $" + price);
-        }
-
-        public void printItem() {
-            System.out.println(this.getName() + " - $" + this.getAdjustedPrice());
-        }
     }
 
+    public void setSize(String size) {
+        this.size = size;
+    }
 
+    public static void printItem(String name, double price) {
+        System.out.printf("%20s:%6.2f%n", name, price);
+    }
 
+    public void printItem() {
+        printItem(getName(), getAdjustedPrice());
+    }
+
+}
